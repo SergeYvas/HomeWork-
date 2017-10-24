9bc4371623949b40920055e1b8be12840b810e8b
SergeyVasilev <ed66679@gmail.com>
2017-10-25 00:53:41 +0300

    divided gulp tasks into different files and improved tab animation

--- app/js/main.js
+++ app/js/main.js
@@ -1,27 +1,20 @@
 'use strict';
 
 
+window.onload = function () {
+    buttonsMenu();
+    navMenu();
+};
 
 
 
-filterOnChilde(document.getElementsByClassName("button-menu_item"));
-
-buttonsMenu();
-noUrl(document.getElementsByClassName('button'));
-noUrl(document.getElementsByClassName('tabs_button'));
-navMenu();
-
-
-function filterOnChilde(data) {
-    for (var i = 0;i<data.length;i++) {
-        if (data[i].childElementCount > 1) {
-            data[i].classList.add('share');
-        }
-    }
-}
 
 
 
+
+///////////////////////////////////
+///////////        common functions
+///////////////////////////////////
 function noUrl(data) {
     for(var i = 0; i < data.length; i++){
         data[i].onclick = function(event) {
@@ -31,7 +24,67 @@ function noUrl(data) {
     return false;
 }}}
 
+function removeClass(data) {
+    for(var i = 0; i < data.length; i++){
+        data[i].classList.remove('active')
+    }
+}
 
+
+
+
+///////////////////////////////////
+///////////        tabs
+///////////////////////////////////
+
+
+tabs();
+function tabs() {
+    var tabsButton  = document.getElementsByClassName('tabs_button'),
+        tabsContent = document.getElementsByClassName('tabs_content'),
+        tabs        = document.querySelector('.tabs');
+
+    addClass();
+    takActiveIndex();
+    noUrl(tabsButton);
+
+    function addClass() {
+        for(var i = 0; i < tabsButton.length; i++)
+        tabsButton[i].addEventListener('click', function(){
+            if (!this.classList.contains('active')){
+                removeClass(tabsButton);
+                this.classList.add('active')
+            }
+        })
+    }
+
+
+    function takActiveIndex() {
+        tabs.addEventListener('click', function (event) {
+            var target = event.target;
+
+            if(target.className === 'tabs_button active'){
+
+                for(var i = 0; i < tabsButton.length; i++){
+                    if(target === tabsButton[i]){
+                        showContent(i);
+                        break;
+                    }
+                }
+            }
+        })
+    }
+
+
+    function showContent(i) {
+        removeClass(tabsContent)
+        tabsContent[i].classList.add('active')
+    }
+}
+
+///////////////////////////////////
+////                   navigation
+///////////////////////////////////
 
 function navMenu() {
     var navItems = document.querySelectorAll('.navigation_item');
@@ -49,27 +102,28 @@ function navMenu() {
     }
 }
 
+///////////////////////////////////
+////  tool buttons and buttons menu
+///////////////////////////////////
 
 function buttonsMenu() {
 
-    var buttons = document.getElementsByClassName('button');
+    var buttons = document.getElementsByClassName('button'),
+        buttonMenuItem  = document.getElementsByClassName("button-menu_item");
 
+    filterOnChilde(buttonMenuItem);
+    noUrl(buttons);
     toggelClass();
+
+
     function toggelClass() {
         for(var i = 0; i < buttons.length; i++){
             buttons[i].addEventListener('click', function () {
                 if (!this.classList.contains('active')){
-                    removeClass();
+                    removeClass(buttons);
                     this.classList.add('active')
-                } else removeClass()
+                } else removeClass(buttons)
             })
-        }
-    }
-
-
-    function removeClass() {
-        for (var i = 0; i < buttons.length;i++){
-            buttons[i].classList.remove('active')
         }
     }
 
@@ -78,11 +132,17 @@ function buttonsMenu() {
         document.addEventListener('click', function () {
             var activeButton = document.querySelector('.button.active');
             if(event.target!==activeButton){
-                removeClass()
+                removeClass(buttons)
             }
         })
     })()
 }
 
-
+function filterOnChilde(data) {
+    for (var i = 0;i<data.length;i++) {
+        if (data[i].childElementCount > 1) {
+            data[i].classList.add('share');
+        }
+    }
+}
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,103 +1,25 @@
 'use strict';
 
-const gulp = require('gulp'),
-      sourcemaps  = require('gulp-sourcemaps'),
-      pug  = require('gulp-pug'),
-      sass = require('gulp-sass'),
-      autoprefixer  = require('gulp-autoprefixer'),
-      csso          = require('gulp-csso'),
-      notify        = require('gulp-notify'),
-      plumber       = require('gulp-plumber'),
-      browserSync = require('browser-sync').create();
+global.$ = {
+            gulp : require('gulp'),
+      sourcemaps : require('gulp-sourcemaps'),
+             pug : require('gulp-pug'),
+            sass : require('gulp-sass'),
+    autoprefixer : require('gulp-autoprefixer'),
+            csso : require('gulp-csso'),
+          notify : require('gulp-notify'),
+         plumber : require('gulp-plumber'),
+     browserSync : require('browser-sync').create(),
 
+    path:{
+           tasks : require('./gulp/config/tasks.js')
+    }
+};
+$.path.tasks.forEach(function (taskPath) {
+    require(taskPath)();
+})
 
-///////// PUG plugons
-gulp.task('pug', function () {
-    return gulp.src(['blocks/*.pug','!blocks/template.pug'])
-        .pipe(plumber())
-        .pipe(pug({
-            pretty: true
-        }))
-        .on("error", notify.onError({
-            message: "Error: <%= error.message %>",
-            title: "HTML"
-        }))
-        .pipe(gulp.dest('app/'));
-});
-
-
-///////// SASS plugins
-gulp.task('sass', function () {
-    return gulp.src('blocks/*.scss')
-        .pipe(plumber())
-        .pipe(sourcemaps.init())
-        .pipe(sass())
-        .pipe(autoprefixer({
-            browsers: ['last 3 versions'],
-            cascade: false
-        }))
-        .on("error", notify.onError({
-            message: "Error: <%= error.message %>",
-            title: "Style"
-        }))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('app/css'));
-
-});
-
-gulp.task('css-prod', function () {
-    return gulp.src('css/*.css')
-        .pipe(plumber())
-        .pipe(sourcemaps.init())
-        .pipe(autoprefixer({
-            browsers: ['last 5 versions'],
-            cascade: false
-        }))
-        .on("error", notify.onError({
-            message: "Error: <%= error.message %>",
-            title: "Style"
-        }))
-        .pipe(csso())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('build/css'));
-
-});
-
-gulp.task('html-prod', function () {
-    return gulp.src('app/*.html')
-        .pipe(plumber())
-        .pipe(sourcemaps.init())
-        .on("error", notify.onError({
-            message: "Error: <%= error.message %>",
-            title: "html"
-        }))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('build/'));
-
-});
-
-
-/////////// BROWSER-SYNC
-gulp.task('server', function() {
-    browserSync.init({
-        server: {
-            baseDir: "app/"
-        }
-    });
-    browserSync.watch("app/", browserSync.reload)
-});
-
-
-/////////// WATCHER
-gulp.task('watch', function () {
-    gulp.watch('blocks/**/*.pug', gulp.series('pug'));
-    gulp.watch('blocks/**/*.scss', gulp.series('sass'))
-});
-
-
-
-///////// DEFAULT
-gulp.task('default', gulp.series(
-    gulp.parallel('pug','sass'),
-    gulp.parallel( 'watch', 'server')
+$.gulp.task('default', $.gulp.series(
+    $.gulp.parallel('pug','sass'),
+    $.gulp.parallel( 'watch', 'server')
 ));
